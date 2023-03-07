2906906c849a72f2f9763516403feea686e1cb7a
neerajbelsare <belsareneeraj@gmail.com>
2023-03-07 16:23:27 +0530

    Added ViewModel

--- app/src/main/java/com/example/preecure/SignInScreen.kt
+++ app/src/main/java/com/example/preecure/SignInScreen.kt
@@ -5,12 +5,20 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme.colors
+import androidx.compose.material.SnackbarDefaults.backgroundColor
+import androidx.compose.ui.res.painterResource
 
 @Preview
 @Composable
@@ -45,14 +53,27 @@ fun SignInScreen(signInViewModel: SignInViewModel = viewModel()) {
                 imeAction = ImeAction.Done
             ),
             visualTransformation = PasswordVisualTransformation(),
-            modifier = Modifier.fillMaxWidth()
+            modifier = Modifier
+                .fillMaxWidth()
         )
         if (signInViewModel.isLoading) {
-            CircularProgressIndicator()
+            Button(
+                onClick = {},
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                CircularProgressIndicator(
+                    modifier = Modifier
+                        .size(30.dp),
+                    color = Color.White,
+                    strokeWidth = 3.dp
+                )
+            }
         } else {
             Button(
                 onClick = { signInViewModel.signIn() },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier
+                    .fillMaxWidth()
             ) {
                 Text("Sign In")
             }
@@ -65,6 +86,53 @@ fun SignInScreen(signInViewModel: SignInViewModel = viewModel()) {
         }
         TextButton(onClick = { /*TODO*/ }) {
             Text(text = "Forgot password?")
+        }
+
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+            .fillMaxWidth()) {
+            Button(
+                modifier = Modifier,
+                elevation = ButtonDefaults.elevation(0.dp, 0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Transparent
+                ),
+                onClick = { /* Handle button click */ }
+            ) {
+                Image(
+                    painterResource(id = R.drawable.google_icon),
+                    contentDescription = "Google Icon"
+                )
+            }
+
+            Button(
+                modifier = Modifier,
+                elevation = ButtonDefaults.elevation(0.dp, 0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Transparent
+                ),
+                onClick = { /* Handle button click */ }
+            ) {
+                Image(
+                    painterResource(id = R.drawable.facebook_icon),
+                    contentDescription = "Facebook Icon"
+                )
+            }
+
+            Button(
+                modifier = Modifier,
+                elevation = ButtonDefaults.elevation(0.dp, 0.dp),
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = Color.Transparent
+                ),
+                onClick = { /* Handle button click */ }
+            ) {
+                Image(
+                    painterResource(id = R.drawable.apple_icon),
+                    contentDescription = "Apple Icon"
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/preecure/SignInViewModel.kt
+++ app/src/main/java/com/example/preecure/SignInViewModel.kt
@@ -14,6 +14,7 @@ class SignInViewModel : ViewModel() {
     var password by mutableStateOf("")
     var isLoading by mutableStateOf(false)
     var isError by mutableStateOf(false)
+    var isEmpty by mutableStateOf(false)
     var errorMessage by mutableStateOf("")
 
     private lateinit var user: FirebaseAuth
@@ -25,21 +26,16 @@ class SignInViewModel : ViewModel() {
         user = FirebaseAuth.getInstance()
 
         if(email.isNotEmpty() && password.isNotEmpty()){
-            user.createUserWithEmailAndPassword(email,password)
-//                .addOnCompleteListener(MainActivity()){
-//                    task ->
-//                if(task.isSuccessful){
-//                    Toast.makeText(MainActivity,"User added succesfully", Toast.LENGTH_SHORT).show()
-//                    startActivity(Intent(this,SignIn::class.java))
-//                    finish()
-//                }else{
-//                    Toast.makeText(this,task.exception!!.message, Toast.LENGTH_SHORT).show()
-//
-//                }
-//            }
-//
-//        }else{
-//            Toast.makeText(this, "Email and password can not be empty", Toast.LENGTH_SHORT)
+            user.createUserWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity()){
+                    task ->
+                if(task.isSuccessful){
+                    isLoading = false
+                }else{
+                    isError = true
+                }
+            }
+        }else{
+            isEmpty = true
         }
     }
 }
