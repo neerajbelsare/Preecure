be8e87d76dc4634ba5c8523362e29c594d10eb66
ShubhamKH08 <hagawaneshubham908@gmail.com>
2023-03-22 11:49:30 +0530

    code for retrival of data

--- app/src/main/java/com/example/preecure/screens/SignupScreen/User.kt
+++ app/src/main/java/com/example/preecure/screens/SignupScreen/User.kt
@@ -1,8 +1,9 @@
 package com.example.preecure.screens.SignupScreen
 
 data class newUser(
-    val name: String,
-    val phoneNumber: String,
-    val email: String,
-    val password: String
+    val username:String ="",
+    val name: String="",
+    val phoneNumber: String="",
+    val email: String="",
+    val password: String=""
 )
